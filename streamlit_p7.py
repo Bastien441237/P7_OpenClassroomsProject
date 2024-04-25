c8d1955b4fae493aeff9c40767387d02f21401d9
Bastien <bastienmoreno37@gmail.com>
2024-04-25 10:18:45 +0200

    Dashboard Streamlit

---
+++ streamlit_p7.py
@@ -0,0 +1,139 @@
+import streamlit as st
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.image as mpimg
+from PIL import Image
+
+# Chemin vers le répertoire d'images
+# images_dir = "C:\\Users\\Basti\\Projets Python\\Machine Learning Engineer\\P7\\Images"
+images_dir = ".\Images"
+
+# Boucle pour récupérer les noms d'images et les labels
+image_files = []
+labels = []
+
+for root, dirs, images in os.walk(images_dir):
+    for image in images:
+        image_files.append(os.path.join(root, image))
+        labels.append(os.path.basename(root).split('-')[1])
+
+# Mise en place des DataFrames
+df_dogs = pd.DataFrame({'image_path': image_files, 'label': labels})
+sample_df = df_dogs.sample(10)
+
+st.sidebar.title("Sommaire")
+pages = ['Contexte du projet', 'Analyse exploratoire des données', 'Nettoyage des données', 'Choix du modèle', 'Prédiction du modèle']
+page = st.sidebar.radio("Aller à la page :", pages)
+
+if page == pages[0]:
+    st.write("### Contexte du projet")
+
+elif page == pages[1]:
+    st.write('### Analyse exploratoire des données')
+
+    # Grouper et compter le nombre d'images par étiquette
+    nb = df_dogs.groupby("label").count().sort_values(by="image_path", ascending=False)
+
+    # Tracer le graphique
+    plt.figure(figsize=(12, 25))
+    sns.set(style="whitegrid")
+    ax = sns.barplot(x="image_path", y=nb.index, data=nb)
+    plt.xlabel("Nombre d'images")
+    plt.ylabel('Labels')
+    plt.title("Nombre d'images par label", fontdict={'fontweight':'bold', 'fontsize':15})
+    plt.tight_layout()
+    st.pyplot(plt)
+
+    # Afficher les images et leurs étiquettes
+    st.write("#### Images aléatoires et leurs étiquettes")
+    plt.figure(figsize=(12, 5))
+    for i in range(10):
+        plt.subplot(2, 5, i + 1)
+        img = mpimg.imread(sample_df.iloc[i]['image_path'])
+        plt.imshow(img)
+        plt.title(sample_df.iloc[i]['label'], fontdict={'fontweight':'bold'})
+        plt.axis('off')  # Supprimer les axes
+    plt.tight_layout()
+    st.pyplot(plt)
+
+    # # Section pour la sélection de données en entrée du moteur de prédiction
+    # st.write("## Sélection de données en entrée")
+    # selected_label = st.selectbox("Sélectionner une étiquette :", df_dogs['label'].unique())
+    # selected_image = df_dogs[df_dogs['label'] == selected_label].sample(1)['image_path'].values[0]
+    # selected_img = mpimg.imread(selected_image)
+    # st.image(selected_img, caption="Image sélectionnée", use_column_width=True)
+
+    # Section pour la sélection de données en entrée du moteur de prédiction
+    st.write("## Sélection de données en entrée")
+    selected_labels = st.multiselect("Sélectionner les étiquettes :", df_dogs['label'].unique())
+    num_images_per_label = st.slider("Nombre d'images par étiquette :", 1, 10, 3)
+
+    # Filtrer le DataFrame en fonction des étiquettes sélectionnées
+    filtered_df = df_dogs[df_dogs['label'].isin(selected_labels)]
+
+    # Afficher les images et leurs étiquettes
+    st.write("Images sélectionnées :")
+    if len(selected_labels) > 0:
+        for label in selected_labels:
+            st.write(f"Images pour l'étiquette '{label}'")
+            images_for_label = filtered_df[filtered_df['label'] == label].sample(num_images_per_label)
+            plt.figure(figsize=(12, 5))
+            for i, (_, row) in enumerate(images_for_label.iterrows(), 1):
+                plt.subplot(1, num_images_per_label, i)
+                img = Image.open(row['image_path'])
+                plt.imshow(img)
+                plt.title(label, fontdict={'fontweight':'bold'})
+                plt.axis('off')  # Supprimer les axes
+            plt.tight_layout()
+            st.pyplot(plt)  # Afficher le graphique dans Streamlit
+    else:
+        st.write("Aucune étiquette sélectionnée. Veuillez en sélectionner au moins une dans la barre latérale.")
+
+elif page == pages[2]:
+    st.write("Nous allons d'abord choisir 15 races de chiens afin de réduire le temps de calcul.")
+    # Liste des étiquettes sélectionnées
+    selected_dirs = ['Bernese_mountain_dog', 'boxer', 'briard',
+                    'Brittany_spaniel', 'bull_mastiff', 'Doberman',
+                    'EntleBucher', 'French_bulldog', 'Gordon_setter',
+                    'Greater_Swiss_Mountain_dog', 'Irish_setter', 'Rottweiler',
+                    'Samoyed', 'Tibetan_terrier', 'vizsla']
+
+    # Section pour la sélection de données en entrée du moteur de prédiction
+    st.write("## Sélection de données en entrée")
+    selected_labels = st.multiselect("Sélectionner les étiquettes :", selected_dirs)
+    num_images_per_label = st.slider("Nombre d'images par étiquette :", 1, 10, 3)
+
+    # Filtrer le DataFrame en fonction des étiquettes sélectionnées
+    filtered_df = df_dogs[df_dogs['label'].isin(selected_labels)]
+
+    # Afficher les images et leurs étiquettes
+    st.write("Images sélectionnées :")
+    if len(selected_labels) > 0:
+        for label in selected_labels:
+            st.write(f"Images pour l'étiquette '{label}'")
+            images_for_label = filtered_df[filtered_df['label'] == label].sample(num_images_per_label)
+            plt.figure(figsize=(12, 5))
+            for i, (_, row) in enumerate(images_for_label.iterrows(), 1):
+                plt.subplot(1, num_images_per_label, i)
+                img = Image.open(row['image_path'])
+                plt.imshow(img)
+                plt.title(label, fontdict={'fontweight':'bold'})
+                plt.axis('off')  # Supprimer les axes
+            plt.tight_layout()
+            st.pyplot(plt)  # Afficher le graphique dans Streamlit
+    else:
+        st.write("Aucune étiquette sélectionnée. Veuillez en sélectionner au moins une dans la barre latérale.")
+
+    st.write("Nous avons ensuite utilisé YOLOV9 afin de pouvoir identifier si des chiens étaient présents sur les images.\
+              En effet, il est possible d'avoir des humains, d'autres animaux ou bien même des objets sur les photos.")
+    # Chemin d'accès de l'image
+    image_path = "n02088094_294.jpg"
+    # Charger l'image à partir du chemin d'accès
+    img = Image.open(image_path)
+    # Afficher l'image dans Streamlit
+    st.image(img, caption='Image', use_column_width=True)
+
+    st.write("Afficher les images une fois cropé sur chiens")
+    st.write("Afficher les images détourées")
